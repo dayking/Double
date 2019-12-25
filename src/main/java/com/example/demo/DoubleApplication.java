ff511880507800e248961be5834badd885a86ac6
panxd <995954532@qq.com>
2019-12-25 14:26:27 +0800

    初始提交

---
+++ src/main/java/com/example/demo/DoubleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleApplication.class, args);
+	}
+
+}
